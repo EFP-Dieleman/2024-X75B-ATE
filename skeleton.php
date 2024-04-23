12a7014c610dd26ffd58e5b15af849cdff57535c
HexMakina <20891495+HexMakina@users.noreply.github.com>
2024-04-23 13:12:26 +0200

    added normalize, content for footer

--- skeleton.php
+++ skeleton.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PROJECT</title>
+
     <link rel="stylesheet" href="X75B.css">
 </head>
 
@@ -24,14 +25,13 @@
                     'product' => 'Item'
                 ];
 
-                foreach($menu as $href => $label){
+                foreach ($menu as $href => $label) {
                     // echo '<li><a href="index.php?page=' . $href . '">' . $label . '</a></li>';
                     printf('<li><a href="index.php?page=%s">%s</a></li>', $href, $label);
-                    
                 }
-                
+
                 ?>
-               
+
             </ul>
         </nav>
     </header>
@@ -47,28 +47,35 @@
     <footer>
         <div>
             <!-- info contact -->
-            X75B
-            02 386 70 58
-            x75b@amstram.Be
-            
-            <!-- socials -->
-            FB
-            X
-            INSTA
-            LINKED
-        </div>
-        
-        <div>
+            <address>
+                X75B<br />
+                02 386 70 58<br />
+                x75b@amstram.Be
+            </address>
 
-            <!-- maps -->
+            <nav>
+                <a href="#">Facebook</a>
+                <a href="#">Instagram</a>
+                <a href="#">X</a>
+            </nav>
         </div>
-            
-        <div>
 
+        <div>
+            <img src="img/map.png" alt="Carte locale de l'EFP">
+        </div>
+
+        <div>
+            <ul>
+                <li><a href="#">Lien 1</a></li>
+                <li><a href="#">Lien 2</a></li>
+                <li><a href="#">Lien 3</a></li>
+                <li><a href="#">Lien 4</a></li>
+            </ul>
             <!-- partenaires/liens/CGV -->
         </div>
 
         <div>
+            &copy; <?php echo date('Y'); ?>
             <!-- copyright -->
         </div>
     </footer>
